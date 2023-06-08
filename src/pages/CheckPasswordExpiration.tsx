d85f199664d5d5d0d78ffef4725de60bd7b9cb6c
NTElissa <ntihindukaelissa77@gmail.com>
2023-06-08 17:55:06 +0200

    new update

--- src/pages/CheckPasswordExpiration.tsx
+++ src/pages/CheckPasswordExpiration.tsx
@@ -9,17 +9,18 @@ function PopupMessage() {
 
   useEffect(() => {
     const popupInterval = setInterval(() => {
-      dispatch(passwordExpirationAction());
-    }, 10000); // 10 seconds 
+      dispatch(passwordExpirationAction () as any);
+    }, 10000); // 1 minute 
 
     return () => clearInterval(popupInterval);
   }, [dispatch]);
 
   console.log("this backend feedback", passwordExpiration);
 
-  if (passwordExpiration && passwordExpiration.expired) {
+  if (passwordExpiration && passwordExpiration.expired===false) {
     return (
-      <div></div> // Don't render anything if the password is expired
+     
+      <div></div>
     );
   }
 
@@ -29,18 +30,21 @@ function PopupMessage() {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
-      <div className="bg-white rounded-lg p-8 shadow-md">
-        <p className="text-center mb-4 border border-gray-300 p-4">
-          <span className="text-red-500 font-bold">Please change your password. It has expired!</span>
-        </p>
-        <p className="text-center mb-4">
-          <span className="text-gray-600">Click here to continue</span>
-        </p>
-        <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded w-full">
-          Change Password
-        </button>
-      </div>
+    <div className="bg-white rounded-lg p-8 shadow-md">
+      <p className="text-center mb-4 border border-gray-300 p-4">
+        <span className="text-red-500 font-bold">Please change your password. It has expired!</span>
+      </p>
+  
+      <p className="text-center mb-4">
+        <span className="text-gray-600">Click here to continue</span>
+      </p>
+      
+      <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded w-full">
+        Change Password
+      </button>
     </div>
+  </div>
+  
   );
 }
 
